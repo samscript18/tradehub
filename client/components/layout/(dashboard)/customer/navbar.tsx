365a39c9de3bb0d41f3595891c4808c60a6dfae3
samscript18 <adeolasam20@gmail.com>
2025-07-11 11:10:41 +0100

    fix: unread notifications count logic ish

--- client/components/layout/(dashboard)/customer/navbar.tsx
+++ client/components/layout/(dashboard)/customer/navbar.tsx
@@ -37,7 +37,7 @@ const CustomerDashboardNavbar = () => {
 				size="icon"
 				className="hover:bg-primary hover:text-white cursor-pointer relative">
 				<Bell className="w-5 h-5" />
-				{typeof unreadNotificationsCount === 'number' && unreadNotificationsCount >= 0 && (
+				{typeof unreadNotificationsCount === 'number' && unreadNotificationsCount > 0 && (
 					<span className="absolute top-1 right-1.5 bg-primary text-white text-xs font-bold rounded-full w-3 h-3 flex items-center justify-center border-2 border-background"></span>
 				)}
 			</Button>
--- client/components/layout/(dashboard)/merchant/navbar.tsx
+++ client/components/layout/(dashboard)/merchant/navbar.tsx
@@ -34,7 +34,7 @@ const MerchantDashboardNavbar = () => {
 					size="icon"
 					className="hover:bg-primary hover:text-white cursor-pointer relative">
 					<Bell className="w-5 h-5" />
-					{typeof unreadNotificationsCount === 'number' && unreadNotificationsCount >= 0 && (
+					{typeof unreadNotificationsCount === 'number' && unreadNotificationsCount > 0 && (
 						<span className="absolute top-1 right-1.5 bg-primary text-white text-xs font-bold rounded-full w-3 h-3 flex items-center justify-center border-2 border-background"></span>
 					)}
 				</Button>
